17d84ae1d118fce3f37cd10a5d601262d680f8a3
ixtdfox <ixtdfox@gmail.com>
2026-03-09 00:04:52 +0300

    Implement interactive phone world map viewport

---
+++ src/ui/screens/phoneMap/worldMapRegions.js
@@ -0,0 +1,7 @@
+export const WORLD_MAP_REGIONS = Object.freeze([
+  { regionId: 'northport', label: 'Northport', x: 348, y: 294 },
+  { regionId: 'larkspur', label: 'Larkspur', x: 672, y: 462 },
+  { regionId: 'emberfall', label: 'Emberfall', x: 930, y: 612 },
+  { regionId: 'sunreach', label: 'Sunreach', x: 1120, y: 388 },
+  { regionId: 'blackreef', label: 'Blackreef', x: 1210, y: 760 }
+]);
